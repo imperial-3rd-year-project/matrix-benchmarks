f23f3fe51752a7294814c6d02f798b0b13090543
Jason Xu <ruiyang.xu18@imperial.ac.uk>
2020-12-30 01:29:15 +0000

    Unrolled with awhile for loop

--- neuralnetwork/Main.hs
+++ neuralnetwork/Main.hs
@@ -13,15 +13,16 @@ import Text.Printf
 
 main :: IO ()
 main = do
+  let epochs = 1000
   let input    = Prelude.reverse [[1, 0], [0, 1], [1, 1], [0, 0]]
   let expected = Prelude.reverse [[1],    [1],    [0],    [0]   ]
   let xorData = [ ((A.fromList (Z :. 2) x :: A.Vector Double), (A.fromList (Z :. 1) y :: A.Vector Double)) | 
                   (x, y) <- Prelude.zip input expected ]
-  let net  = AccNet.create [2, 2, 2, 1]
-  let epochs = 1000
+  let net  = AccNet.create [2, 2, 1]
   putStrLn $ "Begin training for " Prelude.++ show epochs Prelude.++ " epochs (accelerate)"
   start <- getCPUTime
   let net' = AccNet.train net 4 xorData epochs 2
+  -- putStrLn (show net')
   let feedforward' = CPU.run1 (AccNet.feedforward net')
   putStrLn $ show $ feedforward' (A.fromList (Z :. 2) [0, 0] :: A.Vector Double)
   putStrLn $ show $ feedforward' (A.fromList (Z :. 2) [1, 0] :: A.Vector Double)
@@ -32,13 +33,16 @@ main = do
   let diff = (Prelude.fromIntegral (end - start)) / (10 Prelude.^ 12)
   printf "Accelerate: Computation time: %0.3f sec\n" (diff :: Double)
 
-  let xorData = [ (use (A.fromList (Z :. 2) x :: A.Vector Double), use (A.fromList (Z :. 1) y :: A.Vector Double)) | 
-                  (x, y) <- Prelude.zip input expected ]
-  let net  = UnrolledAcc.create [2, 2, 2, 1]
+  let xorData = (A.fromList (Z :. 4 :. 2) (concat input), A.fromList (Z :. 4 :. 1) (concat expected))
+  let net  = run $ UnrolledAcc.create [2, 2, 1]
   putStrLn $ "Begin training for " Prelude.++ show epochs Prelude.++ " epochs (unrolled accelerate)"
-  start <- getCPUTime
-  let net' = UnrolledAcc.train net 4 xorData epochs 2
-  let feedforward' = CPU.run1 (UnrolledAcc.feedforward net')
+  let train = runN (UnrolledAcc.train epochs 4 2)
+      feed  = runN UnrolledAcc.feedforward
+  putStrLn (train `seq` feed `seq` "Finished compilation")
+  start <- train `seq` feed `seq` getCPUTime
+  let net' = train xorData net
+  -- putStrLn (show net')
+  let feedforward' = feed net'
   putStrLn $ show $ feedforward' (A.fromList (Z :. 2) [0, 0] :: A.Vector Double)
   putStrLn $ show $ feedforward' (A.fromList (Z :. 2) [1, 0] :: A.Vector Double)
   putStrLn $ show $ feedforward' (A.fromList (Z :. 2) [1, 1] :: A.Vector Double)
@@ -49,7 +53,7 @@ main = do
   printf "Unrolled Accelerate: Computation time: %0.3f sec\n" (diff :: Double)
   
   let xorData = [ (NLA.fromList i, NLA.fromList e) | (i, e) <- Prelude.zip input expected ]
-  let net  = HNet.create [2, 2, 2, 1]
+  let net  = HNet.create [2, 2, 1]
   putStrLn $ "Begin training for " Prelude.++ show epochs Prelude.++ " epochs (hmatrix)"
   start <- getCPUTime
   let net' = HNet.train net 4 xorData epochs 2
--- neuralnetwork/UnrolledNetwork.hs
+++ neuralnetwork/UnrolledNetwork.hs
@@ -4,17 +4,20 @@
 {-# LANGUAGE UnicodeSyntax #-}
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE TypeOperators #-}
 
 module UnrolledNetwork where
 
-import Data.Array.Accelerate              as A hiding ((!!), length)
+import Data.Array.Accelerate              as A
 import Data.Array.Accelerate.Numeric.LinearAlgebra hiding (trace)
-import Prelude as P
+import Prelude as P hiding ((==), (!!))
+import qualified Prelude as P
 import Data.Array.Accelerate.LLVM.Native as CPU
 
 -- Utilities
 
-evalNet net = undefined -- let BasicNetwork ws bs = net
+-- evalNet net = let BasicNetwork ws bs = net
                 -- in BasicNetwork (P.map (use . CPU.run) ws) (P.map (use . CPU.run) bs)
 
 (^+^) :: (Shape sh, P.Num (Exp c), Elt c) => Acc (Array sh c) -> Acc (Array sh c) -> Acc (Array sh c)
@@ -38,113 +41,115 @@ sigmoid' :: Exp Double -> Exp Double
 sigmoid' = \z -> sigmoid z * (1 - sigmoid z)
 
 type Layer = (Matrix Double, Vector Double)
-data BasicNetwork = BasicNetwork [Matrix Double] [Vector Double]
-  deriving Show
+type BasicNetwork = (Layer, Layer)
 
-
-create :: [Int] -> BasicNetwork
-create xs = BasicNetwork weights biases
+create :: [Int] -> Acc BasicNetwork
+create xs = lift (lift (w1, b1) :: Acc Layer, lift (w2, b2) :: Acc Layer)
   where
-    weights :: [Matrix Double]
-    weights = do
-      idx <- [1..(length xs - 1)]
-      pure $ (fromList ( Z :. xs!!idx :. xs!!(idx - 1) ) [1..] :: Matrix Double)
+    [w1, w2] = do
+      idx <- [1..(P.length xs - 1)]
+      pure $ use $ (fromList ( Z :. xs P.!! idx :. xs P.!! (idx - 1) ) [1..] :: Matrix Double)
     
-    biases :: [Vector Double]
-    biases = do
-      idx <- [1..(length xs - 1)]
-      pure $ (fromList (Z :. xs!!idx) [1..] :: Vector Double)
+    [b1, b2] = do
+      idx <- [1..(P.length xs - 1)]
+      pure $ use $ (fromList (Z :. xs P.!! idx) [1..] :: Vector Double)
 
 
-feedforward :: BasicNetwork -> Acc (Vector Double) -> Acc (Vector Double)
-feedforward (BasicNetwork ws bs) input = res
+feedforward :: Acc BasicNetwork -> Acc (Vector Double) -> Acc (Vector Double)
+feedforward (unlift -> (layer1 :: Acc Layer, layer2 :: Acc Layer)) x = feedLayer layer2 (feedLayer layer1 x)
   where
-    res = feedforward' (P.map use ws) (P.map use bs) input
-    feedforward' :: [ Acc (Matrix Double) ] -> [ Acc (Vector Double) ] -> Acc (Vector Double) -> Acc (Vector Double)
-    feedforward' [] [] a = a
-    feedforward' (w:ws) (b:bs) a = feedforward' ws bs $ A.map sigmoid $ (w #> a) ^+^ b
+    feedLayer :: Acc Layer -> Acc (Vector Double) -> Acc (Vector Double)
+    feedLayer (unlift -> (w :: Acc (Matrix Double), b :: Acc (Vector Double))) x =
+      A.map sigmoid $ (w #> x) ^+^ b
 
-type TrainingData = [ (Acc (Vector Double), Acc (Vector Double)) ]
+type TrainingData = (Matrix Double, Matrix Double)
 
-train' :: Int -> Double
-       -> TrainingData
-       -> Acc Layer
-       -> Acc Layer
-       -> Acc Layer
-       -> Acc (Layer, Layer, Layer)
-train' n eta td layer1 layer2 layer3 = lift (newLayer layer1 n1, newLayer layer2 n2, newLayer layer3 n3)
+trainOnce :: Int -> Double -> Acc TrainingData -> Acc BasicNetwork -> Acc BasicNetwork
+trainOnce n eta (unlift -> (xs :: Acc (Matrix Double), ys :: Acc (Matrix Double)))
+          net@(unlift -> (layer1 :: Acc Layer, layer2 :: Acc Layer)) = lift (layer1', layer2')
   where
-    descend :: TrainingData -> Acc (Layer, Layer, Layer)
-    descend [(x, y)]     = backprop x y layer1 layer2 layer3
-    descend ((x, y):td') = lift (toNabla n1 d1, toNabla n2 d2, toNabla n3 d3)
-      where
-        (n1, n2, n3) :: (Acc Layer, Acc Layer, Acc Layer) = unlift (descend td')
-        (d1, d2, d3) :: (Acc Layer, Acc Layer, Acc Layer) = unlift (backprop x y layer1 layer2 layer3)
-        toNabla :: Acc Layer -> Acc Layer -> Acc Layer
-        toNabla n d = lift (wn ^+^ wd, bn ^+^ bd)
-          where
-            (wn, bn) = unlift n
-            (wd, bd) = unlift d
+    Z :. len :. _ = unlift (shape xs) :: Z :. Exp Int :. Exp Int
+    descend :: Acc (Scalar Int) -> Acc BasicNetwork -> Acc (Scalar Int, BasicNetwork)
+    descend (the -> ix) (unlift -> (layer1 :: Acc Layer, layer2 :: Acc Layer)) =
+      let (w1, b1) = unlift layer1
+          (w2, b2) = unlift layer2
+          sh = lift (Z :. ix :. All)
+          x = slice xs sh
+          y = slice ys sh
+          (layer1' :: Acc Layer, layer2' :: Acc Layer) = unlift (backprop x y net)
+          (w1', b1') = unlift layer1'
+          (w2', b2') = unlift layer2'
+          layer1'' :: Acc Layer = lift (w1 ^+^ w1', b1 ^+^ b1')
+          layer2'' :: Acc Layer = lift (w2 ^+^ w2', b2 ^+^ b2')
+       in lift (unit (ix + 1), lift (layer1'', layer2'') :: Acc BasicNetwork)
 
-    (n1, n2, n3) :: (Acc Layer, Acc Layer, Acc Layer) = unlift (descend td)
+    (w1 :: Acc (Matrix Double), b1 :: Acc (Vector Double)) = unlift layer1
+    (w2 :: Acc (Matrix Double), b2 :: Acc (Vector Double)) = unlift layer2
 
-    velocity :: Exp Double
-    velocity = lift (eta / P.fromIntegral n)
-    newLayer :: Acc Layer -> Acc Layer -> Acc Layer
-    newLayer layer nabla = lift (lw ^-^ (velocity *^ nw), lb ^-^ (velocity *^ nb))
-      where
-        (lw, lb) = unlift layer
-        (nw, nb) = unlift nabla
+    w1Shape = A.shape w1
+    b1Shape = A.shape b1
+    w2Shape = A.shape w2
+    b2Shape = A.shape b2
 
+    w1Zero = A.fill w1Shape 0
+    b1Zero = A.fill b1Shape 0
+    w2Zero = A.fill w2Shape 0
+    b2Zero = A.fill b2Shape 0
 
-train :: BasicNetwork -> Int -> TrainingData -> Int -> Double -> BasicNetwork
-train (BasicNetwork [w1, w2, w3] [b1, b2, b3]) n td epochs eta =
-  let ((w1', b1'), (w2', b2'), (w3', b3')) = loop ((w1, b1), (w2, b2), (w3, b3)) epochs
-   in BasicNetwork [w1', w2', w3'] [b1', b2', b3']
+    zeroLayer1 :: Acc Layer = lift (w1Zero, b1Zero)
+    zeroLayer2 :: Acc Layer = lift (w2Zero, b2Zero)
+
+    zeroNetwork :: Acc BasicNetwork = lift (zeroLayer1, zeroLayer2)
+
+    (nabla1 :: Acc Layer, nabla2 :: Acc Layer) = unlift $ A.asnd $ unlift $
+      awhile (A.uncurry (\(the -> ix) _ -> unit (ix A.< len)))
+        (A.uncurry descend)
+        (lift (unit 0, zeroNetwork))
+
+    velocity = constant (eta / P.fromIntegral n)
+    (nablaW1, nablaB1) = unlift nabla1
+    (nablaW2, nablaB2) = unlift nabla2
+
+    layer1' :: Acc Layer = lift (w1 ^-^ (velocity *^ nablaW1), b1 ^-^ (velocity *^ nablaB1))
+    layer2' :: Acc Layer = lift (w2 ^-^ (velocity *^ nablaW2), b2 ^-^ (velocity *^ nablaB2))
+        
+
+train :: Int -> Int -> Double -> Acc TrainingData -> Acc BasicNetwork -> Acc BasicNetwork
+train (constant -> epochs) n eta td net = A.asnd $
+  awhile (A.uncurry (\(the -> ix) _ -> unit (ix A.< epochs)))
+    (A.uncurry (\(the -> ix) net -> lift (unit $ ix + 1, train' td net))) (lift (unit 0, net))
   where
-    loop net 0 = net
-    loop (l1, l2, l3) epochs =
-      loop (train'' l1 l2 l3) (epochs - 1)
-
-    train'' = runN (train' n eta td)
+    train' = trainOnce n eta
 
 backprop :: Acc (Vector Double)
          -> Acc (Vector Double)
-         -> Acc Layer -> Acc Layer -> Acc Layer
-         -> Acc (Layer, Layer, Layer)
-backprop actual expected layer1 layer2 layer3 = lift (lift (w1', b1') :: Acc Layer
-                                                    , lift (w2', b2') :: Acc Layer,
-                                                      lift (w3', b3') :: Acc Layer)
+         -> Acc BasicNetwork
+         -> Acc BasicNetwork
+backprop actual expected net = lift (lift (y, delta') :: Acc Layer, lift (nw, delta) :: Acc Layer)
   where
-    (w1, b1) = unlift layer1
-    (w2, b2) = unlift layer2
-    (w3, b3) = unlift layer3
-    ws = [w1, w2, w3]
-    bs = [b1, b2, b3]
-    ([b1', b2', b3'], [w1', w2', w3']) = backprop' (P.tail ws) activations zs
-    backprop' :: [Acc (Matrix Double)] 
-              -> [Acc (Vector Double)] 
-              -> [Acc (Vector Double)] 
-              -> ([Acc (Vector Double)], [Acc (Matrix Double)])
-    backprop' [] [a', a] [z] = ([delta], [nw])
-      where
-        delta = (cost' a expected) ^*^ (A.map sigmoid' z)
-        nw = delta >< a'
-    backprop' (w:ws) (a:a':as) (z:zs) = (delta':delta:xs, y:ys)
-      where
-        sp             = A.map sigmoid' z
-        delta'         = ((transpose w) #> delta) ^*^ sp
-        y              = delta' >< a
-        (delta:xs, ys) = backprop' ws (a':as) zs
+    (layer1, layer2) = unlift net
+    (w2 :: Acc (Matrix Double), b2 :: Acc (Vector Double)) = unlift layer2
 
-    (activations, zs) = calcActivations actual ws bs
-    
-    calcActivations x' [] [] = ([x'], [])
-    calcActivations x' (w:ws) (b:bs) = (x':as, z:zs)
+    delta = (cost' a3 expected) ^*^ (A.map sigmoid' z2)
+    nw = delta >< a2
+
+    sp     = A.map sigmoid' z1
+    delta' = (transpose w2 #> delta) ^*^ sp
+    y      = delta' >< a1
+
+
+    activate :: Acc (Vector Double) -> Acc Layer -> Acc (Vector Double)
+    activate x layer = (w #> x) ^+^ b
       where
-        (as, zs) = calcActivations x'' ws bs
-        z        = (w #> x') ^+^ b
-        x''      = A.map sigmoid z
+        (w, b) = unlift layer
+
+    z1, z2 :: Acc (Vector Double)
+    z1 = activate actual layer1
+    z2 = activate a2 layer2
+
+    a1 = actual
+    a2 = A.map sigmoid z1
+    a3 = A.map sigmoid z2
 
 cost' :: Acc (Vector Double) -> Acc (Vector Double) -> Acc (Vector Double)
 cost' actual expected = actual ^-^ expected
